9ff5b7510bc44af3d7275c3bcf8183b0629e194a
Elian Manzueta <eliandmanzueta@protonmail.com>
2024-11-08 14:48:14 -0500

    removed configuration.nix
    Signed-off-by: Elian Manzueta <eliandmanzueta@protonmail.com>

--- hosts/wsl/configuration.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  nix.package = pkgs.nix;
-  nix.settings.experimental-features = "nix-command flakes";
-  nixpkgs.config.allowUnfree = true;
-}
