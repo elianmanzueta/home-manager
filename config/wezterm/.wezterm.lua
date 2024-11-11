ce00264b708837fa1f73eeb94dd247e7a20a3fb2
Elian Manzueta <elianmanzueta@protonmail.com>
2024-11-11 16:06:57 -0500

    updated .wezterm.lua
    Signed-off-by: Elian Manzueta <elianmanzueta@protonmail.com>

--- config/wezterm/.wezterm.lua
+++ config/wezterm/.wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- x86_64-unknown-linux-gnu - Linux
 
 -- WSL
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+if wezterm.running_under_wsl() then
 	config.default_domain = "WSL:Arch"
 end
 
@@ -20,7 +20,18 @@ config.keys = {
 	-- Panes
 	{ key = "s", mods = "CTRL|ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ key = "v", mods = "CTRL|ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+
 	{ key = "w", mods = "CTRL|ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+	{ key = "h", mods = "CTRL|ALT", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "l", mods = "CTRL|ALT", action = wezterm.action.ActivatePaneDirection("Right") },
+
+	{ key = "s", mods = "CTRL", action = wezterm.action.PaneSelect },
+
+  -- Tabs
+	{ key = "{", mods = "ALT", action = wezterm.action.ActivateTabRelative(-1) },
+	{ key = "}", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) },
+	{ key = "t", mods = "SHIFT|ALT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "q", mods = "SHIFT|ALT", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
 }
 
 return config
