cfa4335a5fd6c0f9de64684bd108f9fcbcd25efc
Elian Manzueta <eliandmanzueta@gmail.com>
2024-11-28 12:59:42 -0500

    figured out why config has not been working
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

--- config/doom/config.el
+++ config/doom/config.el
@@ -32,14 +32,15 @@
 
 (after! org
 (custom-set-faces!
-  '(outline-1 :weight extra-bold :height 1.25)
+  '(outline-1 :weight bold :height 1.25)
   '(outline-2 :weight bold :height 1.15)
   '(outline-3 :weight bold :height 1.12)
   '(outline-4 :weight semi-bold :height 1.09)
   '(outline-5 :weight semi-bold :height 1.06)
   '(outline-6 :weight semi-bold :height 1.03)
   '(outline-8 :weight semi-bold)
-  '(outline-9 :weight semi-bold)))
+  '(outline-9 :weight semi-bold)
+  '(org-document-title :weight extra-bold :height 1.5)))
 
 (after! evil-snipe
   (evil-snipe-mode -1)
