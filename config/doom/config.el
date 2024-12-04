aa901feba896694295a1e26ef6dd3ff6ca185106
Elian Manzueta <eliandmanzueta@gmail.com>
2024-12-04 09:35:18 -0500

    emacs stuff
    Signed-off-by: Elian Manzueta <eliandmanzueta@gmail.com>

--- config/doom/config.el
+++ config/doom/config.el
@@ -1,10 +1,30 @@
 (setq user-full-name "Elian Manzueta")
 (setq user-mail-address "elianmanzueta@protonmail.com")
+
 (setq auto-save-default t
       make-backup-files t)
-(setq display-line-numbers-type 'relative)
 (setq confirm-kill-emacs nil)
+(setq display-line-numbers-type nil)
 (setq evil-shift-width 2)
+(setq-default
+ delete-by-moving-to-trash t                      ; Delete files to trash
+ window-combination-resize t                      ; take new window space from all other windows (not just current)
+ x-stretch-cursor t)                              ; Stretch cursor to the glyph width
+
+(setq which-key-idle-delay 0.5)
+
+(setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
+      evil-want-fine-undo t                       ; By default while in insert all changes are one big blob. Be more granular
+      auto-save-default t                         ; Nobody likes to loose work, I certainly don't
+      truncate-string-ellipsis "…"                ; Unicode ellispis are nicer than "...", and also save /precious/ space
+      )
+
+(display-time-mode 1)
+
+(setq doom-scratch-initial-major-mode 'lisp-interaction-mode)
+
+(setq evil-split-window-below t
+      evil-vsplit-window-right t)
 
 (setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 16))
 (setq doom-theme 'doom-monokai-spectrum)
@@ -21,19 +41,21 @@
 (if (eq system-type 'darwin)
     (map! :leader "ot" #'+term/toggle))
 
-(add-hook 'lsp-mode-hook #'lsp-inlay-hints-mode)
+(add-hook 'rust-mode-hook #'lsp-inlay-hints-mode)
 (setq lsp-rust-analyzer-display-chaining-hints t)
 (setq lsp-rust-analyzer-display-closure-return-type-hints t)
 (setq lsp-rust-analyzer-display-parameter-hints t)
 
+(add-hook 'python-mode-hook #'lsp-inlay-hints-mode)
 (setq lsp-pyright-basedpyright-inlay-hints-generic-types t)
 (setq lsp-pyright-basedpyright-inlay-hints-variable-types t)
 (setq lsp-pyright-basedpyright-inlay-hints-call-argument-names t)
 (setq lsp-pyright-basedpyright-inlay-hints-function-return-types t)
 
-(setq lsp-pyright-type-checking-mode "all")
+(setq lsp-pyright-type-checking-mode "strict")
 
 (setq lsp-pyright-venv-path ".")
+(setq lsp-pyright-venv-directory ".venv")
 
 (setq +lookup-open-url-fn #'eww)
 
