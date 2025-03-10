54050f42f5f0c0f2440647f10e153907d0b45f60
Elian Manzueta <elianmanzueta@protonmail.com>
2025-03-10 13:59:45 -0400

    fixed setting environment variable

--- stow/.config/fish/conf.d/display.fish
+++ stow/.config/fish/conf.d/display.fish
@@ -1,3 +1,3 @@
 if test -f /proc/sys/fs/binfmt_misc/WSLInterop
-    set -x DISPLAY=$(route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
+    set -x DISPLAY (route.exe print | grep 0.0.0.0 | head -1 | awk '{print $4}'):0.0
 end
