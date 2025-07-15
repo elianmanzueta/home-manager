a052299e7d83032c6a803f669f077e374f981579
Elian Manzueta <elianmanzueta@protonmail.com>
2025-07-15 19:48:02 -0400

    commit
    Signed-off-by: Elian Manzueta <elianmanzueta@protonmail.com>

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -7,16 +7,11 @@
   home.homeDirectory = "/home/elian";
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  home.packages = [
-    pkgs.hadolint
-    pkgs.nerd-fonts.jetbrains-mono
-  ];
+  home.packages = [ pkgs.hadolint pkgs.nerd-fonts.jetbrains-mono ];
 
   fonts.fontconfig.enable = true;
 
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
+  home.sessionVariables = { EDITOR = "nvim"; };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
