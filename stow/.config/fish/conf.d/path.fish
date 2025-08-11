5ec749f4f95e8348e8553bbccafa227424e9a2dc
Elian Manzueta <elianmanzueta@protonmail.com>
2025-08-10 23:58:09 -0400

    Updated flakes, added path for nix binaries on Mac
    Signed-off-by: Elian Manzueta <elianmanzueta@protonmail.com>

--- stow/.config/fish/conf.d/path.fish
+++ stow/.config/fish/conf.d/path.fish
@@ -4,6 +4,8 @@ switch (uname)
     case Darwin
         # macOS-specific commands
         fish_add_path /Users/elian/.config/emacs/bin
+	fish_add_path /etc/profiles/per-user/elian/bin
+
     case Linux
         # Linux-specific commands
         fish_add_path /home/elian/.local/bin
