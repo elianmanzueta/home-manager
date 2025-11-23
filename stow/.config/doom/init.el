e410686f3cfaa8cc0c72e2e387ae0d0871b2e9ae
Elian Manzueta <elianmanzueta@protonmail.com>
2025-11-23 18:23:49 -0500

    Changes

--- stow/.config/doom/init.el
+++ stow/.config/doom/init.el
@@ -105,7 +105,7 @@
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        pdf               ; pdf enhancements
-       terraform         ; infrastructure as code
+       (terraform +lsp)         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        tree-sitter       ; syntax and parsing, sitting in a tree...
        upload            ; map local nitto remote projects via ssh/ftp
@@ -191,5 +191,5 @@
        ;;(rss +org)        ; emacs as an RSS reader
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens))
