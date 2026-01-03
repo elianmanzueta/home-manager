11238927c484222a9f1c9a2771df0fa2e25b5a34
Elian Manzueta <elianmanzueta@protonmail.com>
2026-01-02 23:55:25 -0500

    wezterm updates

--- stow/.wezterm.lua
+++ stow/.wezterm.lua
@@ -1,30 +1,33 @@
 local wezterm = require("wezterm")
-local config = {}
 
 -- WSL
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_domain = "WSL:Ubuntu"
-end
+-- if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+-- 	config.default_domain = "WSL:Ubuntu"
+-- end
+
+local config = wezterm.config_builder()
+local act = wezterm.action
 
 -- Apperance
 config.color_scheme = "duckbones"
-config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font = wezterm.font("IosevkaTerm Nerd Font Mono")
+config.font_size = 18
 
 -- Keys
 config.keys = {
-	-- Panes
-	{ key = "s", mods = "CTRL|ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "v", mods = "CTRL|ALT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    -- Panes
+    { key = "s", mods = "CTRL|ALT",  action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "v", mods = "CTRL|ALT",  action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 
-	{ key = "w", mods = "CTRL|ALT", action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-	{ key = "h", mods = "CTRL|ALT", action = wezterm.action.ActivatePaneDirection("Left") },
-	{ key = "l", mods = "CTRL|ALT", action = wezterm.action.ActivatePaneDirection("Right") },
+    { key = "w", mods = "CTRL|ALT",  action = act.CloseCurrentPane({ confirm = false }) },
+    { key = "h", mods = "CTRL|ALT",  action = act.ActivatePaneDirection("Left") },
+    { key = "l", mods = "CTRL|ALT",  action = act.ActivatePaneDirection("Right") },
 
-	-- Tabs
-	{ key = "{", mods = "ALT", action = wezterm.action.ActivateTabRelative(-1) },
-	{ key = "}", mods = "ALT", action = wezterm.action.ActivateTabRelative(1) },
-	{ key = "t", mods = "SHIFT|ALT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
-	{ key = "q", mods = "SHIFT|ALT", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+    -- Tabs
+    { key = "{", mods = "ALT",       action = act.ActivateTabRelative(-1) },
+    { key = "}", mods = "ALT",       action = act.ActivateTabRelative(1) },
+    { key = "t", mods = "SHIFT|ALT", action = act.SpawnTab("CurrentPaneDomain") },
+    { key = "q", mods = "SHIFT|ALT", action = act.CloseCurrentTab({ confirm = true }) },
 }
 
 return config
