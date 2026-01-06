c5e6ddb9446f58cf40dcb072764be954789d4f58
Elian Manzueta <elianmanzueta@protonmail.com>
2026-01-06 18:41:48 -0500

    Made fish upgrade function

--- stow/.config/fish/conf.d/abbr.fish
+++ stow/.config/fish/conf.d/abbr.fish
@@ -29,6 +29,34 @@ if type -q emacsclient
     abbr --add orgidp git commit --signoff --message ".orgids"
 end
 
+function upgrade --description "Performs updates on a system."
+    switch (uname)
+        case Linux
+            set distro (cat /etc/os-release | awk -F "=" ' /ID_LIKE/ { print $2 }' | tr -d '"')
+            echo "===============UPDATING PACKAGES==============="
+            if test $distro = arch
+                if type -q paru
+                    paru -Syu --skipreview
+                else
+                    pacman -Syu --noconfirm
+                end
+            end
+
+            if type -q flatpak
+                echo "===============UPDATING FLATPAKS==============="
+                flatpak upgrade -y
+            end
+
+            if type -q doom
+                echo "===============UPDATING DOOM EMACS==============="
+                doom upgrade
+            end
+        case Darwin
+            echo "===============UPDATING PACKAGES==============="
+            brew upgrade
+    end
+end
+
 # Ls
 abbr --add ls eza -lhaF --color=auto --icons=always
 abbr --add l eza -lhaF --color=auto --icons=always
