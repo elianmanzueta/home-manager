5cf61fa0b480300e30f651862add72e779c0554c
Elian Manzueta <elianmanzueta@protonmail.com>
2026-01-27 23:58:15 -0500

    Removed org custom outline faces

--- stow/.config/doom/config.el
+++ stow/.config/doom/config.el
@@ -300,15 +300,7 @@
 (add-hook 'org-mode-hook (lambda () (display-line-numbers-mode -1)))
 
 (custom-set-faces!
-  '(outline-1 :weight bold :height 1.25)
-  '(outline-2 :weight bold :height 1.15)
-  '(outline-3 :weight bold :height 1.12)
-  '(outline-4 :weight semi-bold :height 1.09)
-  '(outline-5 :weight semi-bold :height 1.06)
-  '(outline-6 :weight semi-bold :height 1.03)
-  '(outline-8 :weight semi-bold)
-  '(outline-9 :weight semi-bold)
-  '(org-document-title :weight extra-bold :height 1.5)
+  '(org-document-title :weight extra-bold :height 1.3)
   '(org-verbatim :inherit bold :weight extra-bold))
 
 (use-package! org
