6e6c349bbf9a9dccaff9f0f37bfaa3cf2c70e51b
Elian Manzueta <elianmanzueta@protonmail.com>
2026-03-02 10:28:25 -0500

    Replace setq with setopt, flycheck changes

--- stow/.config/doom/config.el
+++ stow/.config/doom/config.el
@@ -1,3 +1,9 @@
+(use-package flyover
+  :config
+  (setopt flyover-virtual-line-type nil
+        flyover-show-at-eol t
+        flyover-base-height 1))
+
 (with-eval-after-load 'lsp-mode
   (lsp-register-custom-settings
    '(("gopls.hints" ((assignVariableTypes . t)
@@ -12,15 +18,13 @@
   :defer t
   :mode ("justfile\\'" . just-mode)
   :config
-  (setq just-indent-offset 4))
+  (setopt just-indent-offset 4))
 
 (use-package kdl-mode
   :defer t
   :mode ("\\.kdl\\'" . kdl-mode))
 
 (use-package lsp-mode
-  :bind (:map lsp-mode-map
-              ("M-k" . lsp-ui-doc-glance))
   :config
   (lsp-register-client   (make-lsp-client
                           :new-connection (lsp-stdio-connection '("fish-lsp" "start"))
@@ -28,40 +32,45 @@
                           :server-id 'fish-lsp))
   (add-to-list 'lsp-language-id-configuration '(fish-mode . "fish")))
 
+(use-package lsp-ui
+  :bind (:map lsp-ui-doc-mode-map
+              ("M-k" . lsp-ui-doc-glance))
+  :config
+  (setopt lsp-ui-sideline-show-diagnostics nil))
+
+(use-package flycheck
+  :config
+  (flycheck-posframe-configure-pretty-defaults)
+  (setopt flycheck-posframe-mode t))
+
 (use-package powershell
   :mode ("\\.ps1\\'" . powershell-ts-mode)
   :hook (powershell-mode . lsp-mode)
   :config
-  (setq powershell-location-of-exe "/mnt/c/Program Files/Powershell/7/pwsh.exe"))
+  (setopt powershell-location-of-exe "/mnt/c/Program Files/Powershell/7/pwsh.exe"))
 
-(setq lsp-pyright-langserver-command "basedpyright")
+(setopt lsp-pyright-langserver-command "basedpyright")
 
 (use-package flymake-ruff
   :hook (python-mode . flymake-ruff-load))
 
-(setq lsp-rust-analyzer-display-chaining-hints t)
-(setq lsp-rust-analyzer-display-closure-return-type-hints t)
-(setq lsp-rust-analyzer-display-parameter-hints t)
+(setopt lsp-rust-analyzer-display-chaining-hints t)
+(setopt lsp-rust-analyzer-display-closure-return-type-hints t)
+(setopt lsp-rust-analyzer-display-parameter-hints t)
 
 (use-package uv
   :defer t)
 
-(use-package flyover
-  :config
-  (setq flyover-virtual-line-type nil
-        flyover-show-at-eol t
-        flyover-base-height 1))
-
 (use-package dirvish
   :config
-  (setq dirvish-attributes
+  (setopt dirvish-attributes
         '(nerd-icons collapse file-size file-time))
-  (setq dirvish-default-layout '(0 0.11 0.55))
-  (setq dirvish-time-format-string "%d-%m-%y %I:%S:%p %Z")
-  (setq dired-use-ls-dired 't)
-  (setq dirvish-peek-mode 't)
+  (setopt dirvish-default-layout '(0 0.11 0.55))
+  (setopt dirvish-time-format-string "%d-%m-%y %I:%S:%p %Z")
+  (setopt dired-use-ls-dired 't)
+  (setopt dirvish-peek-mode 't)
   (when (and (eq system-type 'darwin) (executable-find "gls"))
-    (setq insert-directory-program "gls")))
+    (setopt insert-directory-program "gls")))
 
 (map! :leader "e" #'dirvish)
 
@@ -70,32 +79,32 @@
   (interactive)
   (dirvish))
 
-(setq doom-theme 'doom-monokai-spectrum)
+(setopt doom-theme 'doom-monokai-spectrum)
 
-(setq doom-font (font-spec :family "IosevkaTerm Nerd Font Mono" :size 18 :weight 'regular))
-(setq doom-emoji-font "Noto Color Emoji")
-(setq doom-symbol-font "Symbols Nerd Font Mono")
+(setopt doom-font (font-spec :family "IosevkaTerm Nerd Font Mono" :size 18 :weight 'regular))
+(setopt doom-emoji-font "Noto Color Emoji")
+(setopt doom-symbol-font "Symbols Nerd Font Mono")
 
-(setq catppuccin-flavor 'mocha
+(setopt catppuccin-flavor 'mocha
       catppuccin-italic-comments t
       catppuccin-italic-variables t
       catppuccin-highlight-matches t)
 
-(setq modus-themes-italic-constructs t)
-(setq modus-themes-bold-constructs t)
-(setq modus-themes-headings
+(setopt modus-themes-italic-constructs t)
+(setopt modus-themes-bold-constructs t)
+(setopt modus-themes-headings
       '((1 . (1.25))
         (2 . (1.15))
         (3 . (1.12))
         (t . (1.05))))
 
-(setq ef-themes-headings
+(setopt ef-themes-headings
       '((1 . (1.25))
         (2 . (1.15))
         (3 . (1.12))
         (t . (1.05))))
 
-(setq kaolin-themes-italic-comments t
+(setopt kaolin-themes-italic-comments t
       kaolin-themes-modeline-padded t)
 
 (defun +eshell-default-prompt-fn ()
@@ -132,7 +141,7 @@
   :config
   (require 'flash-isearch)
   (flash-isearch-mode 1)
-  (setq flash-autojump t
+  (setopt flash-autojump t
         flash-rainbow t
         flash-char-multi-line t
         flash-char-jump-labels t)
@@ -141,10 +150,10 @@
     (evil-global-set-key 'normal (kbd "s") #'flash-evil-jump)
     (evil-global-set-key 'visual (kbd "s") #'flash-evil-jump)))
 
-(setq user-full-name "Elian Manzueta")
-(setq user-mail-address "elianmanzueta@protonmail.com")
+(setopt user-full-name "Elian Manzueta")
+(setopt user-mail-address "elianmanzueta@protonmail.com")
 
-(setq undo-limit 80000000
+(setopt undo-limit 80000000
       confirm-kill-emacs nil
       auto-save-default t
       make-backup-files t
@@ -173,19 +182,19 @@
 (add-hook 'text-mode-hook #'auto-fill-mode)
 (setq-default fill-column 80)
 
-(setq ispell-dictionary "english")
-(setq ispell-personal-dictionary "~/.config/doom/dict/.pws")
+(setopt ispell-dictionary "english")
+(setopt ispell-personal-dictionary "~/.config/doom/dict/.pws")
 
-(setq doom-scratch-initial-major-mode 'lisp-interaction-mode)
-(setq initial-scratch-message "")
-(setq +doom-dashboard-pwd-policy "~/")
+(setopt doom-scratch-initial-major-mode 'lisp-interaction-mode)
+(setopt initial-scratch-message "")
+(setopt +doom-dashboard-pwd-policy "~/")
 
-(setq evil-split-window-below t
+(setopt evil-split-window-below t
       evil-vsplit-window-right t)
 
 (use-package git-auto-commit-mode
   :config
-  (setq! gac-automatically-push-p t
+  (setopt gac-automatically-push-p t
          gac-automatically-add-new-files-p t
          gac-debounce-interval 60
          gac-shell-and " ; and "))
@@ -193,31 +202,33 @@
 (use-package org-agenda
   :after org
   :config
-  (setq org-agenda-timegrid-use-ampm t
+  (setopt org-agenda-timegrid-use-ampm t
         org-display-custom-times t
         org-time-stamp-custom-formats '("<%m/%d/%y %a>" . "<%m/%d/%y %a %I:%M %p>")))
 
 (use-package org-super-agenda
   :after org
   :config
-  (setq org-agenda-start-on-weekday 0)
-  (setq org-super-agenda-header-map (make-sparse-keymap))
-  (setq org-agenda-skip-scheduled-if-done t)
-  (setq org-agenda-skip-deadline-if-done t)
-  (setq org-agenda-overriding-header "")
-  (setq org-agenda-span 14)
+  (setopt org-agenda-start-on-weekday 0)
+  (setopt org-super-agenda-header-map (make-sparse-keymap))
+  (setopt org-agenda-skip-scheduled-if-done t)
+  (setopt org-agenda-skip-deadline-if-done t)
+  (setopt org-agenda-overriding-header "")
+  (setopt org-agenda-span 14)
 
-  (setq org-super-agenda-groups
+  (setopt org-super-agenda-groups
         '((:name ""
            :time-grid t)
           (:name "Projects"
            :and (:children t :tag "projects"))
           (:name "Inbox - Important"
            :and (:tag "inbox" :priority>= "B"))
+          (:name "Inbox - In progress"
+           :and (:tag "inbox" :todo "IN-PROGRESS"))
           (:name "Inbox"
-           :tag "inbox")
+           :and (:tag "inbox" :todo "TODO"))
           (:name "Notes"
-           :tag "note")
+           :todo "NOTE")
           (:discard (:anything t)))))
 
 (add-hook 'org-agenda-mode-hook 'org-super-agenda-mode)
@@ -225,18 +236,18 @@
 (use-package org-attach
   :after org
   :config
-  (setq org-attach-auto-tag nil
+  (setopt org-attach-auto-tag nil
         org-attach-store-link-p 'file
         org-attach-id-to-path-function-list '(org-attach-id-ts-folder-format
                                               org-attach-id-uuid-folder-format
                                               org-attach-id-fallback-folder-format)))
-(setq org-id-method 'ts)
-(setq org-id-ts-format "%Y-%m-%dT%H%M%S.%6N")
+(setopt org-id-method 'ts)
+(setopt org-id-ts-format "%Y-%m-%dT%H%M%S.%6N")
 
 (use-package org-download
   :after org
   :config
-  (setq org-download-image-org-width '450))
+  (setopt org-download-image-org-width '450))
 
 (add-hook 'org-mode-hook 'org-display-inline-images)
 (add-hook 'org-mode-hook (lambda () (hl-line-mode -1)))
@@ -251,7 +262,7 @@
   :bind (:map org-mode-map
               ("M-o" . org-appear-mode))
   :config
-  (setq org-hide-emphasis-markers t
+  (setopt org-hide-emphasis-markers t
         org-fontify-quote-and-verse-blocks t
         org-auto-align-tags nil
         org-tags-column 0
@@ -279,7 +290,7 @@
 (use-package org-modern
   :after org
   :config
-  (setq org-modern-star 'replace
+  (setopt org-modern-star 'replace
         org-modern-replace-stars "◉○✸✿"
         org-modern-block-name nil
         org-modern-timestamp t
@@ -293,7 +304,7 @@
 (use-package org-roam
   :after org
   :config
-  (setq org-roam-node-default-sort 'file-mtime
+  (setopt org-roam-node-default-sort 'file-mtime
         org-roam-file-exclude-regexp (list "~/org/.attach/")))
 
 (defun my/org-roam-node-find-prof ()
@@ -302,7 +313,7 @@
 
 (map! :leader "nrp" 'my/org-roam-node-find-prof)
 
-(setq org-roam-capture-templates
+(setopt org-roam-capture-templates
       '(("d" "default" plain (file "~/org/roam/templates/default.org")
          :if-new (file+head "%<%Y%m%d%H%M%S>-${slug}.org" "#+title: ${title}\n#+author: %n\n#+date: %t\n")
          :unnarrowed t)
@@ -319,7 +330,7 @@
          :unarrowed t)))
 
 (with-eval-after-load 'org
-  (setq org-roam-dailies-capture-templates
+  (setopt org-roam-dailies-capture-templates
         '(("w" "work-todo" plain (file "~/org/roam/templates/work-todo.org")
            :if-new (file+datetree "work-inbox.org" week)
            :unarrowed t))))
@@ -330,27 +341,27 @@
 (use-package org-roam-ui
   :after org-roam
   :config
-  (setq org-roam-ui-follow t
+  (setopt org-roam-ui-follow t
         org-roam-ui-update-on-save t
         org-roam-ui-open-on-start t))
 
 (with-eval-after-load 'org
-  (setq +org-capture-todo-file "inbox.org")
+  (setopt +org-capture-todo-file "inbox.org")
 
-  (setq org-todo-keywords
+  (setopt org-todo-keywords
         '((sequence "TODO(t)" "IN-PROGRESS(i@/!)" "|" "DONE(d!)" "WONT-DO(w@/!)")
           (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D)")
           (sequence "|" "OKAY(o)" "YES(y)" "NO(n)")
           (sequence "NOTE(N)" "HOLD(h)" "|"))))
 
-(setq org-todo-keyword-faces
+(setopt org-todo-keyword-faces
       '(("[-]" . +org-todo-active) ("STRT" . +org-todo-active)
         ("[?]" . +org-todo-onhold) ("WAIT" . +org-todo-onhold)
         ("HOLD" . +org-todo-onhold) ("PROJ" . +org-todo-project)
         ("NO" . +org-todo-cancel) ("KILL" . +org-todo-cancel)
         ("NOTE" . flymake-note-echo)))
 
-(setq org-modern-todo-faces
+(setopt org-modern-todo-faces
       '(("KILL" :inverse-video t :inherit +org-todo-cancel)
         ("NO" :inverse-video t :inherit +org-todo-cancel)
         ("PROJ" :inverse-video t :inherit +org-todo-project)
@@ -361,7 +372,7 @@
         ("NOTE" :inverse-video t :inherit flymake-note-echo)
         ("[-]" :inverse-video t :inherit +org-todo-active)))
 
-(setq explicit-shell-file-name
+(setopt explicit-shell-file-name
       (cond
        ((eq system-type 'darwin) "/opt/homebrew/bin/fish")
        ((eq system-type 'gnu/linux)
@@ -374,17 +385,17 @@
 (use-package vterm
   :defer t
   :init
-  (setq vterm-shell explicit-shell-file-name)
-  (setq vterm-buffer-name-string "vterm: %s"))
+  (setopt vterm-shell explicit-shell-file-name)
+  (setopt vterm-buffer-name-string "vterm: %s"))
 
 (add-load-path! "~/emacs-libvterm")
 
 (use-package eat
   :defer t
   :init
-  (setq process-adaptive-read-buffering nil) ; makes EAT a lot quicker!
-  (setq eat-term-name "xterm-256color") ; https://codeberg.org/akib/emacs-eat/issues/119"
-  (setq eat-shell "/bin/bash"))
+  (setopt process-adaptive-read-buffering nil) ; makes EAT a lot quicker!
+  (setopt eat-term-name "xterm-256color") ; https://codeberg.org/akib/emacs-eat/issues/119"
+  (setopt eat-shell "/bin/bash"))
 
 (add-hook 'eshell-load-hook #'eat-eshell-mode)
 (add-hook 'eshell-load-hook #'eat-eshell-visual-command-mode)
@@ -410,9 +421,9 @@
 (use-package vertico
   :defer t
   :config
-  (setq vertico-buffer-display-action '(display-buffer-reuse-window))
+  (setopt vertico-buffer-display-action '(display-buffer-reuse-window))
 
-  (setq vertico-multiform-categories
+  (setopt vertico-multiform-categories
         '((symbol (vertico-sort-function . vertico-sort-alpha))
           (file (vertico-sort-function . vertico-sort-history-alpha)
                 ))))
